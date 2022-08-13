e25e6c3d447201cf0861ac346ddc5c5a155cb0be
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-08-13 20:42:23 +0700

    remove views engine usage

--- src/app.js
+++ src/app.js
@@ -9,10 +9,6 @@ const { NotFoundHandler, ErrorHandler } = require('@middlewares/error-handler');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

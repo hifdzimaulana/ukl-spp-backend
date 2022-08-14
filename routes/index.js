126005d37738b94ff1dc043ab195f43178567ca7
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-08-14 20:36:38 +0700

    downgrade faker version

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ glob(`${config.folderDir}*${config.ext}`, function (err, files) {
     if (err) {
         throw new Error(err.name)
     } else {
-        console.log(files);
         for (const item of files) {
             var basename = path.basename(item, config.ext)
             var file = require('./' + basename + config.ext)

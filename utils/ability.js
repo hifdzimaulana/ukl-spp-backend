a34a2f527d292c4bd2626ce13e017b30de550d56
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-09-14 23:05:53 +0700

    restrict non-superadmin for updating their level

--- utils/ability.js
+++ utils/ability.js
@@ -1,7 +1,7 @@
 const { Ability, AbilityBuilder } = require('@casl/ability')
 
 const abilities = (id, role) => {
-    const { can, build } = new AbilityBuilder(Ability)
+    const { can, cannot, build } = new AbilityBuilder(Ability)
 
     switch (role) {
         case 'superadmin':
@@ -12,6 +12,7 @@ const abilities = (id, role) => {
             can('create', 'Pembayaran')
             can(['update', 'delete'], 'Pembayaran', { idPetugas: id })
             can('update', 'Petugas', { id })
+            cannot('update', 'Petugas', 'level')
             can('manage', 'Kelas')
             can('manage', 'Siswa')
             break;

eebe8d9d2dcb3c6d11187362ec7583ab381795be
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-09-14 16:09:58 +0700

    new user abilities definition

--- utils/ability.js
+++ utils/ability.js
@@ -1,22 +1,21 @@
 const { Ability, AbilityBuilder } = require('@casl/ability')
-const { Kelas, Pembayaran, Petugas, Siswa, Spp } = require('@models')
 
 const abilities = (id, role) => {
-    const { can, cannot, build } = new AbilityBuilder(Ability)
+    const { can, build } = new AbilityBuilder(Ability)
 
     switch (role) {
         case 'superadmin':
-            can('manage', [Kelas, Pembayaran, Petugas, Siswa, Spp])
+            can('manage', 'all')
             break;
         case 'admin':
-            can('create', [Pembayaran])
-            can('read', [Kelas, Pembayaran, Petugas, Siswa, Spp])
-            can('update', [Pembayaran], { idPetugas: id })
-            can('update', Petugas, { id })
-            can('delete', [Pembayaran], { idPetugas: id })
+            can('create', 'Pembayaran')
+            can('read', ['Kelas', 'Pembayaran', 'Siswa', 'Spp'])
+            can('update', 'Pembayaran', { idPetugas: id })
+            can(['read', 'update'], 'Petugas', { id })
+            can('delete', 'Pembayaran', { idPetugas: id })
             break;
         case 'owner':
-            can('read', [Kelas, Pembayaran, Petugas, Siswa, Spp])
+            can('read', 'all')
             break;
         default:
             break;
